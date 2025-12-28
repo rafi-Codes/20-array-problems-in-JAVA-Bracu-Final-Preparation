dee6a547503ea8ebd52b3348b7f0330182234316
Mohammad Rafiul Hasan <rafihasan27rafi@gmail.com>
2025-12-28 09:19:39 +0600

    Update K times right shift.java

--- K times right shift.java
+++ K times right shift.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+import java.util.Arrays;
 
+public class codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        k %= arr.length; // avoid unnecessary iteration, like if 5 size array is changed 5 times, it becomes the same as before.
+        while (k--> 0) {
+            for (int i = arr.length - 1; i > 0 ; i--) {
+                int temp = arr[i];
+                arr[i] = arr [i - 1];
+                arr[i - 1] = temp;  
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+        sc.close();
+    }
+}
